5332a2b012177d71df090c29ffff2c8e60f61015
Adil Naimi <adilnm23@hotmail.com>
2020-06-09 03:25:29 -0500

    first commit

---
+++ stuff.js
@@ -0,0 +1,14 @@
+var counter=function(arr){
+    return 'There are '+ arr.length+ ' elements'
+}
+
+var adder=function(a,b){
+    return `The some of the 2 numbers is ${a+b}`
+}
+
+var pi=3.142
+module.exports={
+    counter,
+    adder,
+    pi
+}
