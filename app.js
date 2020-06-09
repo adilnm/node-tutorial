559397ee04d5463682a03e26b22347441bd703c2
Adil Naimi <adilnm23@gmail.com>
2020-06-09 10:02:46 -0500

    events 2

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var events=require('events')
+var util= require('util')
 
-var myEmitter= new events.EventEmitter()
+var Person=function(name){
+    this.name=name
+}
 
-myEmitter.on('someEvent', function(msg){
-    console.log(msg)
+util.inherits(Person, events.EventEmitter)
+
+var james=new Person('james')
+var mary=new Person('mary')
+var ryu=new Person('ryu')
+var people=[james,mary,ryu]
+
+people.forEach(function(person){
+    person.on('speak', function(msg){
+        console.log(person.name+' said '+msg)
+    })
 })
 
-myEmitter.emit('someEvent','the event was emmited')
+james.emit('speak', 'hey dudes')
+mary.emit('speak', 'hello')
+
