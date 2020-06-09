4dc53e9604fa5c14da0dabd525f6fc8185a97893
Adil Naimi <adilnm23@gmail.com>
2020-06-09 10:22:01 -0500

    Reading and writing files Async

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
 
-var readMe = fs.readFileSync("readMe.txt", "utf8");
+fs.readFile("readMe.txt", "utf8", function(err, data){
+    fs.writeFile('writeMe.txt', data, err=>{})
+});
 
-fs.writeFileSync('writeMe.txt', readMe)
+
