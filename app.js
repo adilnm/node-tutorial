5b332e0cc094326a395ae49818ce7c14dc303fc5
Adil Naimi <adilnm23@gmail.com>
2020-06-09 22:00:01 -0500

    Setup server

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var fs = require("fs");
+var http = require("http");
 
-fs.readFile("readMe.txt", "utf8", function(err, data){
-    fs.writeFile('writeMe.txt', data, err=>{})
+var server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hey ninjas");
 });
 
+server.listen(3000, "127.0.0.1");
 
+console.log('Now listening to port 3000')
