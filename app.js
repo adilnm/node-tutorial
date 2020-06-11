2f3cd2916b99a1a02cb6e06af64df1832d55197f
Adil Naimi <adilnm23@gmail.com>
2020-06-11 04:33:45 -0500

    Using pipes to send info to writed streams

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var http = require("http");
 var fs = require("fs");
 
-var myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
-var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
-myReadStream.on("data", function(chunk) {
-  console.log("new chunk");
-  myWriteStream.write(chunk);
+var server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  var myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
+  myReadStream.pipe(res);
 });
-// var server = http.createServer((req, res) => {
-//   res.writeHead(200, { "Content-Type": "text/plain" });
-//   res.end("Hey ninjas");
-// });
 
-// server.listen(3000, "127.0.0.1");
+server.listen(3000, "127.0.0.1");
 
-// console.log('Now listening to port 3000')
+console.log("Now listening to port 3000");
