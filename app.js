6dfa01ba345b2c02d826a6a53b6322608f1e15db
Adil Naimi <adilnm23@gmail.com>
2020-06-11 05:18:58 -0500

    Routing

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ var http = require("http");
 var fs = require("fs");
 
 var server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
-  var myObj = {
-    name: "Ryu",
-    job: "Ninja",
-    age: 29
-  };
-  res.end(JSON.stringify(myObj))
+  console.log("Request: " + req.url);
+  if (req.url === "/home" || req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/index.html").pipe(res);
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/contact.html").pipe(res);
+  } else if (req.url === "/api/ninjas") {
+    var ninjas = [
+      { name: "ryu", age: 29 },
+      { name: "Yoshi", age: 32 }
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(ninjas));
+  }else{
+    res.writeHead(404, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/404.html").pipe(res);
+  }
 });
 
 server.listen(3000, "127.0.0.1");
