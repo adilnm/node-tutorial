2640d93edd25ba6c58d37a26de5ce8c389ea8936
Adil Naimi <adilnm23@gmail.com>
2020-06-11 04:59:33 -0500

    Serve json to the client

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var http = require("http");
 var fs = require("fs");
 
 var server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
+  res.writeHead(200, { "Content-Type": "application/json" });
   var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
-  myReadStream.pipe(res);
+  var myObj = {
+    name: "Ryu",
+    job: "Ninja",
+    age: 29
+  };
+  res.end(JSON.stringify(myObj))
 });
 
 server.listen(3000, "127.0.0.1");
