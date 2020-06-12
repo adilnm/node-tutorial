3acac84d7e93579b9c6c4e5741cadaba1d9eedb3
Adil Naimi <adilnm23@gmail.com>
2020-06-12 17:57:23 -0500

    Setup ejs package

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ var express = require("express");
 
 var app = express();
 
+app.set("view engine", 'ejs');
 app.get("/", function(req, res) {
-  res.send('this is the home page')
+  res.sendFile(__dirname + "/index.html");
 });
 app.get("/contact", function(req, res) {
-  res.send('this is the contact page')
+  res.sendFile(__dirname + "/contact.html");
 });
 
-app.get("/profile/:id", function(req,res){
-  res.send('You requested to see a profile with id of: '+req.params.id)
-})
+app.get("/profile/:id", function(req, res) {
+  res.render("profile");
+});
 app.listen(3000);
