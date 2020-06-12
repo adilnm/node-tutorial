aa86933fb8fd83f946c453d313ab8721f9bfab87
Adil Naimi <adilnm23@gmail.com>
2020-06-11 23:27:54 -0500

    Setup dynamic route

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.get("/", function(req, res) {
 app.get("/contact", function(req, res) {
   res.send('this is the contact page')
 });
+
+app.get("/profile/:id", function(req,res){
+  res.send('You requested to see a profile with id of: '+req.params.id)
+})
 app.listen(3000);
